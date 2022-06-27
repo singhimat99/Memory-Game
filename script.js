1f8f2fe6f41c5894f864cec38c1ac1facb8fb4ef
Himat Singh <singhimat99@gmail.com>
2022-06-27 16:26:51 -0700

    improved randomize function, added hiding and revealing on click features, next is checking win

--- script.js
+++ script.js
@@ -1,24 +1,62 @@
 "use strict";
-const resetGame = document.querySelector(".resetGame");
-let random;
+const reset = document.querySelector(".resetGame");
+const images = document.querySelectorAll(".img");
+const containers = document.querySelectorAll(".container");
 let numbers = [];
-function randomizeImages() {
-  const images = document.querySelectorAll(".img");
+
+function randomize() {
   let unique = [];
   let i = 0;
-  while (unique.length != 6) {
-    random = Math.round(Math.random() * 5);
+  while (unique.length != 3) {
+    let random = Math.round(Math.random() * 2);
     numbers[i] = random;
     i++;
     unique = Array.from(new Set(numbers));
   }
-  for (let j = 0; j < unique.length; j++) {
-    if (images[j].src !== `img${unique[j] + 1}.png`) {
-      //   console.log(images[j].src + " before");
-      images[j].src = `img${unique[j] + 1}.png`;
-      //   console.log(images[j].src);
-    }
-  }
-  unique = [];
+  return unique;
 }
-resetGame.addEventListener("click", randomizeImages);
+
+function reassignImages() {
+  let unique = randomize();
+  let arr2 = randomize();
+  for (let i = 0; i < arr2.length; i++) {
+    unique.push(arr2[i]);
+  }
+  for (let j = 0; j < unique.length; j++) {
+    images[j].src = `img${unique[j] + 1}.png`;
+    // if (images[j].src !== `img${unique[j] + 1}.png`) {
+    //   images[j].src = `img${unique[j] + 1}.png`;
+    //   console.log(images[j].src + " before");
+    //   console.log(images[j].src);
+  }
+}
+
+function hideImages() {
+  for (let i = 0; i < images.length; i++) {
+    images[i].style.visibility = "hidden";
+  }
+}
+
+function resetGame() {
+  reassignImages();
+  hideImages();
+}
+resetGame();
+reset.addEventListener("click", resetGame);
+
+for (let i = 0; i < containers.length; i++) {
+  containers[i].addEventListener("click", function () {
+    if (images[i].style.visibility === "hidden") {
+      images[i].style.visibility = "visible";
+    } else {
+      images[i].style.visibility = "hidden";
+    }
+  });
+}
+// function revealImage(i) {
+//   if (images[i].style.visibility === "hidden") {
+//     images[i].style.visibility = "visible";
+//   } else {
+//     images[i].style.visibility = "hidden";
+//   }
+// }
